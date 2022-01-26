e32181b83ae5d3b330efd623c74e43865910e4a7
ItsShuriken <55306099+ItsShuriken@users.noreply.github.com>
2022-01-26 17:23:14 -0600

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Project1 {
+
+	public static void main (String[] args) {
+		
+		String file ="Airtable.csv";
+		BufferedReader reader = null;
+		String line = "";
+		
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			while((line = reader.readLine()) != null) {
+				String[] row = line.split(",");
+				
+				for(String index : row) {
+					System.out.printf("%-10s", index);
+				}
+				System.out.println();
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
