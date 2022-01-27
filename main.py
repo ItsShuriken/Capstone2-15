ce2dfcbb5b8642851228738b8d25ec87bc67e4be
ItsShuriken <clark.g.smith-1@ou.edu>
2022-01-27 19:28:54 +0000

    created functions and main, still need tests

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 import csv
 
-filename = '/Repo/Capstone2-15/input.txt'
+filename = 'input.txt'
 
 fields = []
 rows = []
 
 
-with open(filename, 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+def read_csv(filename):
+    with open(filename, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
     
-    fields = next(csv_reader)
+        fields = next(csv_reader)
     
-    for row in csv_reader:
-        rows.append(row)
+        for row in csv_reader:
+           rows.append(row)
         
     
-    print("Total number of rows: %d"%(csv_reader.line_num))
+        print("Total number of rows: %d"%(csv_reader.line_num))
     
 
-print('Field names are:' + ', '.join(field for field in fields))
+    print('Field names are:' + ', '.join(field for field in fields))
 
 
-print('\nFirst 5 rows:\n')
-for row in rows[:5]:
+    print('\nFirst 5 rows:\n')
+    for row in rows[:5]:
     
-    for col in rows:
-        print("%10s"%col),
-    print('\n')
+        for col in rows:
+            print("%10s"%col),
+        print('\n')
+        
+        
+def main():
+    read_csv(filename)
+    
+    
+if __name__ == "__main__":
+    main()
