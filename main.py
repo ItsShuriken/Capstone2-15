535b8a21a069e4c52658b600f42996de1d2e317f
ItsShuriken <clark.g.smith-1@ou.edu>
2022-02-01 00:32:16 +0000

    added return statement for read function, adjusted test assert

--- main.py
+++ main.py
@@ -1,53 +1,34 @@
 import csv
-import unittest
+import pytest
 
 filename = 'input.txt'
 
-fields = []
 rows = []
 
 
-def read_csv(filename):
-    with open(filename, 'r', newline='') as csv_file:
+def read_csv(file):
+    with open(file, 'r', newline='') as csv_file:
         csv_reader = csv.reader(csv_file)
-    
-        fields = next(csv_reader)
-    
+        header = next(csv_reader)
+        
         for row in csv_reader:
-           rows.append(row)
-        
-    
-       # print("Total number of rows: %d"%(csv_reader.line_num))
-    
-
-    #print('Field names are:' + ', '.join(field for field in fields))
-
-
-    #print('\nFirst 5 rows:\n')
-
-   # for col in rows[:5]:
-   #     print("%10s"%col),
-  #  print('\n')
-        
-
-
-class TestCSV(unittest.TestCase):
+            print (' '.join(row))
+            rows.append(row)
+            
+        for info in rows:
+            print("%s"%info)
+    return rows
     
     
-    def test_csv_read(self):
-        
-        expected = [['A', 'A'], ['B', 'B'], ['C', 'C'], ['D', 'CH'], ['E', 'D'], ['F', 'E'], ['G', 'F'], ['H', 'G'], 
-        ['I', 'H'], ['J', 'I'], ['K', 'J'], ['L', 'K'], ['M', 'L'], ['N', 'LL'], ['O', 'M'], ['P', 'N'], ['Q', 'Ñ'], ['R', 'O'], 
-        ['S', 'P'], ['T', 'Q'], ['U', 'R'], ['V', 'S'], ['W', 'T'], ['X', 'U'], ['Y', 'V'], ['Z', 'W'], ['a', 'X'], ['b', 'Y'], ['c', 'Z'], 
-        ['d', 'a'], ['e', 'b'], ['f', 'c'],  ['g', 'ch'], ['h', 'd'], ['i', 'e'], ['j', 'f']]
-        self.assertEqual(expected, rows)
-        
+def test_csv_read():
     
-    
+    expected = [['A', 'A'], ['B', 'B'], ['C', 'C'], ['D', 'CH'], ['E', 'D'], ['F', 'E'], ['G', 'F'], ['H', 'G'], ['I', 'H'], ['J', 'I'], ['K', 'J'], ['L', 'K'], ['M', 'L'], ['N', 'LL'], ['O', 'M'], ['P', 'N'], ['Q', 'Ñ'], ['R', 'O'], ['S', 'P'], ['T', 'Q'], ['U', 'R'], ['V', 'S'], ['W', 'T'], ['X', 'U'], ['Y', 'V'], ['Z', 'W'], ['a', 'X'], ['b', 'Y'], ['c', 'Z'], ['d', 'a'], ['e', 'b'], ['f', 'c'], ['g', 'ch'], ['h', 'd'], ['i', 'e'], ['j', 'f'], ['k', 'g'], ['l', 'h'], ['m', 'i'], ['n', 'j'], ['o', 'k'], ['p', 'l'], ['q', 'll'], ['r', 'm'], ['s', 'n'], ['t', 'ñ'], ['u', 'o'], ['v', 'p'], ['w', 'q'], ['x', 'r'], ['y', 's'], ['z', 't'], ['', 'u'], ['', 'v'], ['', 'w'], ['', 'x'], ['', 'y'], ['', 'z']]
+    assert read_csv(filename) == expected
+
 
 def main():
     read_csv(filename)
-    unittest.main()
+   # unittest.main()
     
     
 if __name__ == "__main__":
